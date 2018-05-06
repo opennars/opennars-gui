a783d6041aba5cc2d6b8fb435307fc3b7a262da6
Tony Lofthouse <tony.lofthouse@gmilab.com>
2018-05-06 16:58:44 +0100

    Update package.java

--- src/main/java/org/opennars/gui/package.java
+++ src/main/java/org/opennars/gui/package.java
@@ -1,4 +1,4 @@
-/*
+;/*
 *    This program is free software: you can redistribute it and/or modify
 *    it under the terms of the GNU General Public License as published by
 *    the Free Software Foundation, either version 3 of the License, or
@@ -18,4 +18,4 @@
 *
 
 */
-package org.opennars.gui
+package org.opennars.gui;
