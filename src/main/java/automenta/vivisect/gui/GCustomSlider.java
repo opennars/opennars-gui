24ff4c51807008b567b736dfa9064769d58cb2f2
PtrMan <robertw89@googlemail.com>
2018-08-01 03:35:40 +0200

    Docs: forgot file

--- src/main/java/automenta/vivisect/gui/GCustomSlider.java
+++ src/main/java/automenta/vivisect/gui/GCustomSlider.java
@@ -77,7 +77,7 @@ import processing.core.PImage;
  * </ul>
  * Where ??? is the image type file extension. The image type can be any that Processing can handle, the
  * most common types will be png, jpg or gif but tga is also permitted
- * 
+ *
  * 
  * <p>There are very few restrictions about the images you use but when designing the images you should consider
  * the following facts:</p>
